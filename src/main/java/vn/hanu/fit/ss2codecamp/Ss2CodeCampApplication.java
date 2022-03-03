817bba85d57dbad8cfa7b87edfd3f2b8031e32ee
chiendq <chiendq.work@gmail.com>
2022-03-03 21:14:18 +0700

    Initial commit

---
+++ src/main/java/vn/hanu/fit/ss2codecamp/Ss2CodeCampApplication.java
@@ -0,0 +1,13 @@
+package vn.hanu.fit.ss2codecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2CodeCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2CodeCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/hanu/fit/ss2codecamp/Ss2CodeCampApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hanu.fit.ss2codecamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2CodeCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
