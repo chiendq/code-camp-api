600651b1d47650c8049958927f20d31aca972628
chiendq <chiendq.work@gmail.com>
2022-03-06 12:28:52 +0700

    capture standard output stream from other class

--- src/main/java/vn/hanu/fit/ss2codecamp/controller/TestController.java
+++ src/main/java/vn/hanu/fit/ss2codecamp/controller/TestController.java
@@ -1,26 +1,55 @@
 package vn.hanu.fit.ss2codecamp.controller;
 
-import org.aspectj.apache.bcel.classfile.Code;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import vn.hanu.fit.ss2codecamp.TestClass;
 import vn.hanu.fit.ss2codecamp.utils.FileUtils;
 
-import java.io.IOException;
+import javax.print.attribute.standard.PresentationDirection;
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @RestController
 
 public class TestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
     private static final String PATH = "src/main/java/vn/hanu/fit/ss2codecamp/TestClass.java";
 
+    private static final String ABSOLUTE_PATH = "D:\\Y3S2\\Software Engineer 2\\project\\Sample\\SE2-Project-master\\se2-tut3-springboot-starter\\ss2-code-camp\\src\\main\\java\\vn\\hanu\\fit\\ss2codecamp\\TestClass.java";
     @PostMapping("/test")
-    public String testAPI(@RequestBody  String code) throws IOException {
-        LOGGER.info("POST REQUEST /test: ");
+    public ResponseEntity<?> testAPI(@RequestBody  String code) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         FileUtils.init(code, PATH);
-        return "OK";
+
+
+        Class<?> clazz = Class.forName("vn.hanu.fit.ss2codecamp.TestClass");
+        Method main = clazz.getDeclaredMethod("main", String[].class);
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(out);
+            main.invoke(main, new Object[]{new String[0]});
+            System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    public void testTestTest(){
+        String expect = "Hello World";
     }
 
 
